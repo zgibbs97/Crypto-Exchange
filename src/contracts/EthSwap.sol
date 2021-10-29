4057fe85671247c764f4ed62d8850cbe38258ad0
Zachary Gibbs <zachary.gibbs15@gmail.com>
2021-10-29 08:31:56 -0600

    Adding buytoken functions and tests

--- src/contracts/EthSwap.sol
+++ src/contracts/EthSwap.sol
@@ -6,9 +6,18 @@ import "./Token.sol";
 contract EthSwap {
     string public name = "EthSwap Instant Exchange";
     Token public token;
-
+    uint public rate = 100;
+    
     constructor(Token _token) public {
-        token = _token;
+         token = _token;
     }
+    
+    function buyTokens() public payable {
+        // Redemption rate = # of tokens they receive for 1 ether
+        // Amount of Eth * Rate
+        // Calculate number of tokens to buy
+        uint tokenAmount = msg.value * rate;
+        token.transfer(msg.sender, tokenAmount);
+    }
+    
 }
-
--- test/EthSwap.test.js
+++ test/EthSwap.test.js
@@ -8,20 +8,19 @@ require("chai")
   .use(require("chai-as-promised"))
   .should();
 
-  function tokens(n) {
-    return web3.utils.toWei(n, 'ether')
-  }
+function tokens(n) {
+  return web3.utils.toWei(n, "ether");
+}
 
-contract("EthSwap", (accounts) => {
-    let token, ethSwap
-        
-    before(async () => {
-        token = await Token.new()
-        ethSwap = await EthSwap.new()
-        //Transfer all tokens to ethswap (1 Million) '1000000' => '1000000000000000000000000' "
-        await token.transfer(ethSwap.address, tokens('1000000'))
-    })
+contract("EthSwap", ([deployer, investor]) => {
+  let token, ethSwap;
 
+  before(async () => {
+    token = await Token.new();
+    ethSwap = await EthSwap.new(token.address);
+    //Transfer all tokens to ethswap (1 Million) '1000000' => '1000000000000000000000000' "
+    await token.transfer(ethSwap.address, tokens("1000000"));
+  });
 
   describe("Token deployment", async () => {
     it("contract has a name", async () => {
@@ -36,9 +35,27 @@ contract("EthSwap", (accounts) => {
       assert.equal(name, "EthSwap Instant Exchange");
     });
 
-    it('contract has token', async () => {
-        let balance = await token.balanceOf(ethSwap.address)
-        assert.equal(balance.toString(), tokens('1000000'))
-    })
+    it("contract has token", async () => {
+      let balance = await token.balanceOf(ethSwap.address);
+      assert.equal(balance.toString(), tokens("1000000"));
+    });
   });
-});
+
+  describe("buyTokens()", async () => {
+    let result
+    before(async () => {
+      // Purchase tokens before each example
+      result = await ethSwap.buyTokens({
+        from: investor,
+        value: web3.utils.toWei("1", "ether"),
+      });
+    });
+
+    it("Allows user to instantly purchase tokens from ethSwap for a fixed price", async () => {
+      // Check investor token balance after purchase
+      let investorBalance = await token.balanceOf(investor)
+      assert.equal(investorBalance.toString(), tokens('100'))
+      });
+    });
+  });
+
